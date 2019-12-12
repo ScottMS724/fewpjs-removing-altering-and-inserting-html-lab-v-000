c32e205a555d4fe3d694396e5c4ef36af31a65d0
Scott Silverman <scottms724@gmail.com>
2019-12-12 17:33:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ let newHeader = document.createElement('h1');
 
 newHeader.setAttribute("id", "victory");
 
-newHeader.innerHTTML = "YOUR-NAME is the champion";
+newHeader.innerHTTML = "Scott is the champion";
